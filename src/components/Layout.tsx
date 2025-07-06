15aac7fb20977b7573681d617935c27b93f9ee0b
Daric Teske <tesked@outlook.com>
2025-07-06 13:08:50 -0500

    fix auth

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,22 @@
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, useLocation, useNavigate } from "@tanstack/react-router";
 import { Navbar } from "./Navbar";
+import useAuth from "../hooks/useAuth";
+import { useEffect } from "react";
 
 export function Layout() {
+  const { session } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!session?.user && location.pathname !== "/") {
+      navigate({ to: "/" });
+    }
+    if (session?.user && location.pathname === "/") {
+      navigate({ to: "/totes" });
+    }
+  }, [session, navigate, location.pathname]);
+
   return (
     <div className="h-screen bg-base-100">
       <Navbar />
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,30 +1,12 @@
-import {
-  createRootRoute,
-  useLocation,
-  useNavigate,
-} from "@tanstack/react-router";
+import { createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Layout } from "../components/Layout";
-
-import useAuth from "../hooks/useAuth";
 
 export const Route = createRootRoute({
   component: RootRoute,
 });
 
 function RootRoute() {
-  const { session } = useAuth();
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-
-  if (session && pathname === "/") {
-    navigate({ to: "/totes", replace: true });
-  }
-
-  // if (!session && pathname !== "/") {
-  //   navigate({ to: "/", replace: true });
-  // }
-
   return (
     <>
       <Layout />
